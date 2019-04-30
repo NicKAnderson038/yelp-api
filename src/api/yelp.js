c7d1036ea7f9885160a3ab3f421731b0364e077f
NicKAnderson038 <andersonnick038@gmail.com>
2019-04-30 12:18:48 -0700

    update with info

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,25 @@
+'use strict'
+
 const express = require('express')
+const yelp = require('yelp-fusion')
 
 const router = express.Router()
+const client = yelp.client(process.env.API_KEY)
+
+let data
+client
+  .business(process.env.CLIENT_ID)
+  //   .reviews(process.env.CLIENT_ID)
+  .then(response => {
+    console.log(response.jsonBody.reviews[0].text)
+    data = response.jsonBody.reviews[0].text
+  })
+  .catch(e => {
+    console.log(e)
+  })
 
 router.get('/', (req, res) => {
-  res.json(['😀', '😳', '🙄'])
+  res.json(data)
 })
 
 module.exports = router
